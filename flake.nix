5c4093821d0e10d013fdfe6aa0be1aa279237a7d
Nathan Guerrero <naguerr2@uci.edu>
2026-02-05 09:55:46 +0000

    cleanup: Remove docker-host-pve1

--- flake.nix
+++ flake.nix
@@ -56,20 +56,6 @@
             })
           ];
         };
-        # "docker-host-pve1" = nixpkgs.lib.nixosSystem { # Docker host on node pve1
-        #   specialArgs = {inherit inputs outputs;};
-        #   system = "x86_64-linux";
-        #   modules = [
-        #     # Config-specific files
-        #     instances/docker-host-pve1/hardware-configuration.nix
-        #     (import modules/disko-types/impermanence-btrfs.nix { device = "/dev/sda"; })
-        #     (import machines/docker-host/configuration.nix {
-        #       secretsFile = "${./instances/docker-host-pve1/secrets.yaml}"; 
-        #       instanceValues = builtins.fromTOML (builtins.readFile "${./instances/docker-host-pve1/instance-values.toml}"); 
-        #       constantsValues = builtins.fromTOML (builtins.readFile "${./constants/homelab-constants-values.toml}"); 
-        #     })
-        #   ];
-        # };
         "docker-host-pve3" = nixpkgs.lib.nixosSystem { # Docker host on node pve3
           specialArgs = {inherit inputs outputs;};
           system = "x86_64-linux";
