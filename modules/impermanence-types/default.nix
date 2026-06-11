0f07310e7b51e1bea17d334f7fcc964182f7dcd2
Nathan Guerrero <naguerr2@uci.edu>
2026-06-11 00:07:06 -0700

    fix: Prefix /bin/ for each binary run in script of the impermanence-root-rollback service in the Impermanence Module

--- modules/impermanence-types/default.nix
+++ modules/impermanence-types/default.nix
@@ -50,28 +50,28 @@
     serviceConfig.Type = "oneshot";
 
     script = ''
-      mkdir /btrfs_tmp
-      mount /dev/root_vg/root /btrfs_tmp
+      /bin/mkdir /btrfs_tmp
+      /bin/mount /dev/root_vg/root /btrfs_tmp
       if [[ -e /btrfs_tmp/root ]]; then
-          mkdir -p /btrfs_tmp/old_roots
-          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+          /bin/mkdir -p /btrfs_tmp/old_roots
+          timestamp=$(/bin/date --date="@$(/bin/stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+          /bin/mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
       fi
 
       delete_subvolume_recursively() {
           IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+          for i in $(/bin/btrfs subvolume list -o "$1" | /bin/cut -f 9- -d ' '); do
               delete_subvolume_recursively "/btrfs_tmp/$i"
           done
-          btrfs subvolume delete "$1"
+          /bin/btrfs subvolume delete "$1"
       }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+      for i in $(/bin/find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
           delete_subvolume_recursively "$i"
       done
 
-      btrfs subvolume create /btrfs_tmp/root
-      umount /btrfs_tmp
+      /bin/btrfs subvolume create /btrfs_tmp/root
+      /bin/umount /btrfs_tmp
     '';
     # script = ''
     #   ${pkgs.coreutils}/bin/mkdir /btrfs_tmp
